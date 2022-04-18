47810b401d7c720d381349ba60a1775a86e7962c
AJ <abhandal@umd.edu>
2022-04-18 10:45:26 +1000

    Make and commit Project

---
+++ src/main/java/com/bhandal/springmicroservicebeers/SpringMicroserviceBeersApplication.java
@@ -0,0 +1,13 @@
+package com.bhandal.springmicroservicebeers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceBeersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceBeersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bhandal/springmicroservicebeers/SpringMicroserviceBeersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhandal.springmicroservicebeers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroserviceBeersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
